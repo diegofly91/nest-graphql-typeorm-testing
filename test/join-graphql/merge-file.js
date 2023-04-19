9d54c02614640c95dd6249a9358cb05107e6b75d
diegofly91 <diego_fernandolibreros@hotmail.com>
2023-04-18 19:46:56 -0500

    edit changest test file

--- test/join-graphql/merge-file.js
+++
@@ -1,9 +0,0 @@
-const { loadFilesSync } = require('@graphql-tools/load-files');
-const { mergeTypeDefs } = require('@graphql-tools/merge')
-const { print } = require( 'graphql');
-const fs = require('fs')
-
-const loadedFiles = loadFilesSync(`src/modules/**/graphql/*.graphql`);
-const typeDefs = mergeTypeDefs(loadedFiles);
-const printedTypeDefs = print(typeDefs);
-fs.writeFileSync(`test/join-graphql/joined.graphql`, printedTypeDefs);
